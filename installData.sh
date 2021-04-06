2eb41d8e111026190fe1a818b6616ec28a8bcac7
Jerome <Royan>
2021-04-06 19:49:05 +0200

    Update install scripts

--- installData.sh
+++ installData.sh
@@ -4,4 +4,14 @@ mkdir -p data/fbow_voc
 unzip -o fbow_voc.zip -d ./data/fbow_voc
 rm fbow_voc.zip
 
+# Download TUM camera calibration
+curl https://artifact.b-com.com/solar-generic-local/captures/singleRGB/TUM/tum_camera_calibration.yml -L -o data/tum_camera_calibration.yml
+
+# Download TUM video for testing relocalization
+curl https://artifact.b-com.com/solar-generic-local/captures/singleRGB/TUM/rgbd_dataset_freiburg3_long_office_household_relocalization.avi -L -o data/rgbd_dataset_freiburg3_long_office_household_relocalization.avi
+
+# Download TUM map for testing relocalization
+curl https://artifact.b-com.com/solar-generic-local/maps/TUM/freiburg3_long_office_household/map_linux_0_9_1.zip -L -o map.zip
+unzip -o map.zip -d ./data
+rm map.zip
 
