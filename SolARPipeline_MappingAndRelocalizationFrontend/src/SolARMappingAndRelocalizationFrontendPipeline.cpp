79c5fab3261b109da1b6fe8531d88b7200b22035
Christophe Cutullic <christophe.cutullic@b-com.com>
2023-03-30 14:09:56 +0200

    Fix bug cumulated distance && add disparity tolerance when init transform

--- SolARPipeline_MappingAndRelocalizationFrontend/src/SolARMappingAndRelocalizationFrontendPipeline.cpp
+++ SolARPipeline_MappingAndRelocalizationFrontend/src/SolARMappingAndRelocalizationFrontendPipeline.cpp
@@ -58,6 +58,7 @@ SolARMappingAndRelocalizationFrontendPipeline::SolARMappingAndRelocalizationFron
         declareProperty("thresholdTranslationRatio", m_thresTranslationRatio);
         declareProperty("minCumulativeDistance", m_minCumulativeDistance);
         declareProperty("thresholdRelocConfidence", m_thresRelocConfidence);
+        declareProperty("poseDisparityToleranceInit", m_poseDisparityTolerance);
 
         LOG_DEBUG("All component injections declared");
 
@@ -1326,12 +1327,12 @@ void SolARMappingAndRelocalizationFrontendPipeline::processRelocalization()
             // test reloc pose by comparing it to device pose, reject the reloc if big difference is observed
             if (clientContext->m_mappingStatus != BOOTSTRAP) {
                 auto poseArrInSolar = clientContext->m_T_M_SolAR*pose;
+                LOG_DEBUG("Client pose in SolAR coordinates: \n{}", poseArrInSolar.matrix());
                 Vector3f dist(poseArrInSolar(0, 3)-new_pose(0, 3), poseArrInSolar(1, 3)-new_pose(1, 3), poseArrInSolar(2, 3)-new_pose(2, 3));
                 LOG_DEBUG("Pose distance = {} / cumulative distance = {} / min cumulative distance = {} / ratio = {} / cumulative distance*ration = {}",
                          dist.norm(), clientContext->m_cumulativeDistance, m_minCumulativeDistance, m_thresTranslationRatio, clientContext->m_cumulativeDistance*m_thresTranslationRatio);
                 if ((clientContext->m_cumulativeDistance > m_minCumulativeDistance) && (dist.norm() > clientContext->m_cumulativeDistance*m_thresTranslationRatio)) {
-                    LOG_WARNING("SolAR reloc pose is rejected because translation vector too different from that in AR runtime pose");
-                    clientContext->m_cumulativeDistance = 0.f; // reset cumulative distance
+                    LOG_INFO("Reject reloc pose since distance to device pose is {} on cumulated distance {} ", dist.norm(), clientContext->m_cumulativeDistance);
                     return;
                 }
             }
@@ -1344,8 +1345,8 @@ void SolARMappingAndRelocalizationFrontendPipeline::processRelocalization()
             }
 
             LOG_INFO("Transformation matrix from client to SolAR:\n{}", (new_pose * pose.inverse()).matrix());
-            findTransformation(clientContext, new_pose * pose.inverse());
-            clientContext->m_cumulativeDistance = 0.f; // reset cumulative distance when relocalized
+            if (findTransformation(clientContext, new_pose * pose.inverse()))
+                clientContext->m_cumulativeDistance = 0.f; // reset cumulative distance when relocalized
         }
     }  catch (const exception &e) {
         LOG_ERROR("Exception raised during remote request to the relocalization service: {}", e.what());
@@ -1515,13 +1516,24 @@ void SolARMappingAndRelocalizationFrontendPipeline::processMapping()
     }
 }
 
-void SolARMappingAndRelocalizationFrontendPipeline::findTransformation(const SRef<ClientContext> clientContext,
+bool SolARMappingAndRelocalizationFrontendPipeline::findTransformation(const SRef<ClientContext> clientContext,
                                                                        Transform3Df transform)
 {
     unique_lock<mutex> lock(clientContext->m_mutexFindTransform);
     clientContext->m_vector_reloc_transf_matrix.push_back(transform);
 	// find mean transformation
     if (clientContext->m_vector_reloc_transf_matrix.size() == m_nbRelocTransformMatrixRequest) {
+        if (clientContext->m_T_M_SolAR.isApprox(Transform3Df::Identity()) && clientContext->m_vector_reloc_transf_matrix.size() >= 2) {
+            for (auto i = 1; i<clientContext->m_vector_reloc_transf_matrix.size(); i++) {
+                for (int d = 0; d < 3; d++) {
+                    if (std::abs( clientContext->m_vector_reloc_transf_matrix[0](d, 3) - clientContext->m_vector_reloc_transf_matrix[i](d, 3) ) > m_poseDisparityTolerance) {
+                        clientContext->m_vector_reloc_transf_matrix.clear();
+                        return false;
+                    }
+                }
+            }
+        }
+
         Transform3Df transform3D = transform3DAverage(clientContext->m_vector_reloc_transf_matrix);
 
         LOG_INFO("Mean transformation matrix from device to SolAR:\n{}", transform3D.matrix());
@@ -1541,7 +1553,10 @@ void SolARMappingAndRelocalizationFrontendPipeline::findTransformation(const SRe
         clientContext->m_relocTimer.restart();
         clientContext->m_isNeedReloc = false;
         clientContext->m_vector_reloc_transf_matrix.clear();
+        return true;
 	}
+    else
+        return false;
 }
 
 /// @brief check if need to relocalize
